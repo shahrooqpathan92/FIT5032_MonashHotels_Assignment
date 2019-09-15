57301a33e96a5e1405526798672e1745f8869f01
Shahrooq Pathan <spat0023@student.monash.edu>
2019-09-15 16:25:23 +1000

    Added basic Registration and login

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_MonashHotels_Assignment.Startup))]
+namespace FIT5032_MonashHotels_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
